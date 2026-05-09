6a69cae891d6ce3873a6f3d0037bd623e89a0d9e
Pranesh <praneshwaran574@gmail.com>
2026-05-09 19:25:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -10,8 +10,10 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == 0:
                 zero += 1
+
             elif nums[i] == 1:
                 one += 1
+
             else:
                 two += 1
 
@@ -19,13 +21,10 @@ class Solution:
             if zero != 0:
                 nums[i] = 0
                 zero -= 1
-
             elif one != 0:
                 nums[i] = 1
                 one -= 1
-            
             else:
                 nums[i] = 2
                 two -= 1
-
         
