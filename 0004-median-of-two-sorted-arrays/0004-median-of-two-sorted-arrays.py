ba5be2ee91e75fc81bc3fbf6a40f3931e4cd6e4c
Pranesh <praneshwaran574@gmail.com>
2026-06-28 11:32:30 +0530

    Time: 6 ms (15.62%), Space: 19.4 MB (94.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,14 +1,39 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums = nums1 + nums2
-        nums.sort()
-        
-        if len(nums) % 2 != 0:
-            return float(nums[len(nums)//2])
+        n1 = len(nums1)
+        n2 = len(nums2)
 
-        else:
-            mid = len(nums) // 2
-            value = (nums[mid] + nums[mid-1]) / 2
-            return float(value)
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2,nums1)
 
-    
+        l = 0
+        h = n1
+        left = (n1 + n2 + 1) // 2
+        n = n1 + n2
+
+        while l <= h:
+            mid1 = (l + h) // 2
+            mid2 = left - mid1
+            l1 = l2 = float('-inf')
+            r1 = r2 = float('inf')
+
+            if mid1 < n1:
+                r1 = nums1[mid1]
+            if mid2 < n2:
+                r2 = nums2[mid2]
+
+            if mid1 - 1 >= 0: l1 = nums1[mid1-1]
+            if mid2 - 1 >= 0: l2 = nums2[mid2-1]
+
+            if l1 <= r2 and l2 <= r1:
+                if n % 2 != 0:
+                    return max(l1,l2)
+
+                else:
+                    return (max(l1,l2) + min(r1,r2)) / 2
+
+            elif l1 > r2:
+                h = mid1 - 1
+
+            else:
+                l = mid1 + 1
