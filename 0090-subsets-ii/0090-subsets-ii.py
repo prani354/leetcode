c986180fa7431faed4508e2a41aba19490051b01
Pranesh <praneshwaran574@gmail.com>
2026-07-03 00:29:04 +0530

    Time: 0 ms (100%), Space: 19.4 MB (88.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,22 +1,25 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        
         res = []
+        nums.sort()
+        curr = []
 
-        def backtrack(i,ans):
-            if i == len(nums):  #Base case
-                res.append(ans[:])
+        def backtrack(i):
+            if i == len(nums):
+                res.append(curr[:])
                 return
 
-            ans.append(nums[i])  #include duplicates in left half
-            backtrack(i+1,ans)
+            #Include the number nums[i]
 
-            while i+1 < len(nums) and nums[i] == nums[i+1]:
+            curr.append(nums[i])
+            backtrack(i+1)
+            curr.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
                 i += 1
 
-            ans.pop()  #Exclude duplicates
-            backtrack(i+1,ans)
+            backtrack(i+1) #To add the empty set which means not included nums[i]
 
-        backtrack(0,[])
+        backtrack(0)
         return res
-
