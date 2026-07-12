4c91a5283428f580121e78e89bdd55d5604ca54c
Pranesh <praneshwaran574@gmail.com>
2026-07-12 12:39:07 +0530

    Time: 3 ms (63.67%), Space: 19.3 MB (45.2%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -9,19 +9,16 @@ class Solution:
             hashmap[ch] = hashmap.get(ch,0) + 1
 
         for ch in s:
-
+            
             hashmap[ch] -= 1
 
             if ch in visited:
-                continue #Skip
+                continue
 
             while stack and ch < stack[-1] and hashmap[stack[-1]] > 0:
-                visited.remove(stack.pop())
+                 visited.remove(stack.pop())
 
             visited.add(ch)
             stack.append(ch)
 
         return "".join(stack)
-
-        
-
