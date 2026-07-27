a1b186d8d373b0e1d32ca545d065ae4760c370ed
Pranesh <praneshwaran574@gmail.com>
2026-07-27 14:16:21 +0530

    Time: 8 ms (41%), Space: 21.1 MB (85.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
+        candidate = 0
+        count = 0
 
-        return nums[len(nums)//2]
+        #Moore Voting algorithm
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
