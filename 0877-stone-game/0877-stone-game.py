ad5aec0aa4be5ad67e6c87f21faac5302ee4a4be
Pranesh <praneshwaran574@gmail.com>
2026-08-02 18:49:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (55.87%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,13 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        n = len(piles)
+
+        if n % 2 == 0: return True
+
+        dp = list(piles)
+
+        for i in range(n-2,-1,-1):
+            for j in range(i+1,n):
+                dp[j] = max(nums[i]-dp[j],nums[j]-dp[j+1])
+
+        return dp[-1] >= 0
