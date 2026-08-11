a9134fb00edd65d80a6b3b9b2657f3ce2cf4b1b9
Pranesh <praneshwaran574@gmail.com>
2026-08-11 12:36:05 +0530

    Time: 198 ms (39.6%), Space: 34.8 MB (77.85%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,33 +1,39 @@
 from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        output = []
+        # n = len(nums)
+        # q = deque(nums[:k])  It is a TLE solution but this also correct solution
+
+        # res = []
+        # res.append(max(q))
+        # #print(res)
+        # r = len(q)
+        # while r < n:
+        #     q.popleft()
+        #     q.append(nums[r])
+        #     res.append(max(q))
+        #     r += 1
+
+        # return res
+
+        # Optimized solution
         q = deque()
+        n = len(nums)
+        res = []
 
-        if not nums:
-            return []
+        if not nums: return []
+        if k == 1: return nums
 
-        if k == 1:
-            return nums
-
-        for i in range(len(nums)):
-            #validity range
-
+        for i in range(n):
             while q and q[0] < i-k+1:
-                q.popleft()
+                q.popleft() # Validating the k-range
             
-            #popping out the minimum value
-            while q and nums[q[-1]] < nums[i]:
+            while q and nums[q[-1]] < nums[i]: # Stack like to remove minimum value
                 q.pop()
 
-            q.append(i)
+            q.append(i) #Idx queue
 
-            #appending the maximum value from each window
-            if i >= k-1:
-                output.append(nums[q[0]])
+            if i >= k - 1:
+                res.append(nums[q[0]])
 
-        return output
-
-            
-
-        
+        return res
