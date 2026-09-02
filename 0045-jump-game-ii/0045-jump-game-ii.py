88654ed48921886d997a4f8c209a422d5585460f
Pranesh <praneshwaran574@gmail.com>
2026-09-02 12:08:48 +0530

    Time: 6 ms (55.98%), Space: 20 MB (65.72%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        reach,count,last = 0,0,0
+        far = 0
+        count = 0
+        last = 0
 
         for i in range(len(nums)-1):
-            reach = max(reach,i+nums[i])
+            far = max(far,i+nums[i])
 
             if i == last:
-                last = reach
+                last = far
                 count += 1
 
         return count
