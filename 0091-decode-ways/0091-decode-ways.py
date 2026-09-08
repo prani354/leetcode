4431a48d3e766c9777db0feddfa30623df3f5e61
Pranesh <praneshwaran574@gmail.com>
2026-09-08 11:16:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (71.02%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -11,6 +11,6 @@ class Solution:
                 dp[i] = dp[i+1]
 
             if (i+1 < n and (s[i] == '1' or s[i] == '2' and s[i+1] in "0123456")):
-                dp[i] = dp[i] + dp[i+2]
+                dp[i] += dp[i+2]
 
         return dp[0]
